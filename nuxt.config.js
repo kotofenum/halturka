6c34a5635452eb9d4ff0e4bbc862cde3d8977e8e
Alexander Khristo <alexander.khristo@rentateam.ru>
2019-12-29 15:52:49 +0300

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,9 +12,33 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
-      }
+      },
+      { name: 'apple-mobile-web-app-title', content: 'Constafit' },
+      { name: 'application-name', content: 'Constafit' },
+      { name: 'msapplication-TileColor', content: '#603cba' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+    ]
   },
   /*
    ** Customize the progress-bar color
